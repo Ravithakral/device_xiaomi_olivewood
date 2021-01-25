d74525506d3d65c6835e13f0a6812211ec830dc6
Chenyang Zhong <zhongcy95@gmail.com>
2021-01-25 05:24:14 -0500

    pine: create and symlink a dummy /metadata/apex
    Google moved apex sessions directory from /data/apex/sessions to
    /metadata/apex/sessions after commit:
    
    "Move apex sessions directory to /metadata"
    https://android.googlesource.com/platform/system/apex/+/36cf4bbac6108898a531aacc102dcc489d791b37
    
    Devices with a mounted metadata partition will have the needed
    directories set up by system/core/rootdir/init.rc. Xiaomi devices
    on sdm845 do not have a metadata partition out of the box, so things
    like "Google Play system update" will fail to install the update.
    
    Therefore, create a dummy directory under /data/vendor/metadata_apex
    and symlink it to /metadata/apex.
    
    The reason why the old /data/apex/sessions directory is not used
    for the symlink is that apexd will call migrateSessionsDirIfNeeded()
    to recursively copy things from the old directory to the new one.
    Creating the symlink from /data/apex/sessions may result in
    unintended behaviors.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- device.mk
+++ device.mk
@@ -275,7 +275,8 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    apex_metadata.rc
 
 PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
