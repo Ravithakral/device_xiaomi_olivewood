bf71b11e3c97165d4ede0c00436e203dc0484e4b
Jarl-Penguin <jarlpenguin@outlook.com>
2021-07-20 19:18:15 +0530

    Olivewood: build libvndkfwk_detect from source

--- device.mk
+++ device.mk
@@ -309,7 +309,10 @@ PRODUCT_PACKAGES += \
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
-    libqti_vndfwk_detect
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti \
+    libvndfwk_detect_jni.qti.vendor
 
 # Ramdisk
 PRODUCT_PACKAGES += \
