dcfff7bd7f19a0058d2e38ac8e0401a9db287344
Rahul K <41164942+rk134@users.noreply.github.com>
2021-09-18 16:29:21 +0100

    olivewood: Havoc-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_olivewood.mk
+    $(LOCAL_DIR)/havoc_olivewood.mk
+
+COMMON_LUNCH_CHOICES := \
+    havoc_olivewood-user \
+    havoc_olivewood-userdebug \
+    havoc_olivewood-eng
