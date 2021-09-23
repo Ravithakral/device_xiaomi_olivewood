87983aa821d200568fb34fb0a582cb0e872d0871
a7imranjha <65493194+a7imranjha@users.noreply.github.com>
2021-09-23 11:44:19 +0500

    olivewood: evolve

--- arrow_olivewood.mk
+++ arrow_olivewood.mk
@@ -12,29 +12,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from olivewood device
 $(call inherit-product, device/xiaomi/olivewood/device.mk)
 
-# Inherit some common ArrowOS stuff
+# Inherit some common Evolution X stuff
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
-$(call inherit-product, vendor/arrow/config/common.mk)
-
-# flags
-#CUSTOM_BUILD_TYPE := UNOFFICIAL
-TARGET_USES_BLUR := true
-TARGET_INCLUDE_PIXEL_CHARGER := true
-IS_PHONE := true
-TARGET_FACE_UNLOCK := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
-TARGET_INCLUDE_STOCK_ARCORE := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+EVO_BUILD_TYPE := UNOFFICIAL
+EVO_MAINTAINER := ASIM_RANJHA
+TARGET_GAPPS_ARCH := arm64
+WITH_GAPPS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_olivewood
+PRODUCT_NAME := evolution_olivewood
 PRODUCT_DEVICE := olivewood
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 8A/8A Dual
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-DEVICE_MAINTAINER := Asim_Ranjha
 
