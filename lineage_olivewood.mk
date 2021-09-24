fb876d979a1962b93b0eaeb43bb1770ac5634482
ASHISH11948 <90844148+ASHISH11948@users.noreply.github.com>
2021-09-24 11:16:17 +0530

    Update lineage_olivewood.mk

--- lineage_olivewood.mk
+++ lineage_olivewood.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The PixelPlusUI Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,15 +12,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from olivewood device
 $(call inherit-product, device/xiaomi/olivewood/device.mk)
 
-# Inherit some common Evolution X stuff
+# Inherit some aosp stuff.
 TARGET_BOOT_ANIMATION_RES := 720
-TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-EVO_BUILD_TYPE := UNOFFICIAL
-EVO_MAINTAINER := ASIM_RANJHA
-TARGET_GAPPS_ARCH := arm64
-WITH_GAPPS := true
+
+# PixelPlusUI flags
+#CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 720
+#TARGET_SUPPORTS_GOOGLE_RECORDER := true
+#TARGET_INCLUDE_STOCK_ARCORE := true
+#TARGET_INCLUDE_LIVE_WALLPAPERS := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_olivewood
@@ -28,5 +31,8 @@ PRODUCT_DEVICE := olivewood
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 8A/8A Dual
 PRODUCT_MANUFACTURER := Xiaomi
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=olivewood
